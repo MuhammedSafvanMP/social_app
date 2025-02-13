af1f46e9c12fbd448ed5cc8c05d0eac7dd5af153
MUHAMMED SAFVAN MP <muhammedsafvanmp7@gmail.com>
2025-02-13 18:05:11 +0530

    feat: (Navbar added)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import {ClerkProvider} from '@clerk/nextjs'
 import { ThemeProvider } from "@/components/ThemeProvider";
+import Navbar from "@/components/Navbar";
 
 
 const geistSans = Geist({
@@ -27,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning >
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
@@ -37,7 +38,22 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            {children}
+            <div className="min-h-screen">
+              <Navbar />
+            
+              <main className="py-8">
+                <div className="max-w-7xl mx-auto px-4">
+                  <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
+                    <div className="hidden lg:block lg:col-span-3">
+                      side bar
+                    </div>
+                    <div className="lg:col-span-9">
+                    {children}
+                    </div>
+                  </div>
+                </div>
+              </main>
+            </div>
           </ThemeProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,10 @@
 import React from 'react'
-import {
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
-import { Button } from "@/components/ui/button"
-import ModeToggle from '@/components/ModeToggle'
 
 
 export default function Home() {
   return (
     <>
-    <SignedOut>
-    <SignInButton >
-    <Button variant="outline">Sign In</Button>
-      </SignInButton> 
-  </SignedOut>
-  <SignedIn>
-    <UserButton />
-  </SignedIn>
-  <ModeToggle />
+      <h1>home page</h1>
     </>
   )
 }
